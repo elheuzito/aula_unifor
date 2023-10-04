8fc2e8ca5fa6e21aef5b7ce8cf888db2411bde1b
Filipe <filipegosson650@gmail.com>
2023-10-04 20:18:14 -0300

    Alterei texto do produto

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/cadastro/:nome?", function(req,res){
     //req --> dados enviados pelo cliente
     var nome = req.params.nome;
     if (nome){
-        res.send("<h1>produto " + nome + " criado!</h1>");
+        res.send("<h1>produto 1" + nome + " criado!</h1>");
     }else{
         res.send("produto criado!");
     }
